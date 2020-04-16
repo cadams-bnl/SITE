0ee1549a42ca17ebf38cdd7caf92fdc75bc7c114
Christian Adams <christian.adams@bnl-consulting.com>
2020-04-15 23:22:36 -0400

    added readme

---
+++ src/Page2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Page2 extends React.Component {
+    list = {
+        one: '1',
+        two: '2',
+        three: '3'
+    }
+    render() {
+        return (
+            <div>
+                <h> page2 </h>
+            </div>
+        )
+    }
+}
+export default Page2;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import Page2 from './Page2'
 import * as serviceWorker from './serviceWorker';
 
+const rootEl = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/Page2' component={Page2} />
+    </Switch>
+  </BrowserRouter>,
+  rootEl
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
